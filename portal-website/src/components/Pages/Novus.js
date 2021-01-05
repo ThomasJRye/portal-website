66c9f3d606e8d7a7581532303516e22934a20447
thr998 <47362421+thr998@users.noreply.github.com>
2021-01-05 15:18:12 +0100

    deleted src by accident

--- portal-website/src/components/Pages/Novus.js
+++ portal-website/src/components/Pages/Novus.js
@@ -42,7 +42,7 @@ const Novus = () => {
                 <div className = 'join-Button'>
                     {Button && <Button 
                         buttonStyle='btn--outline' 
-                        destination='/novus-form'
+                        destination='/nero-form'
                         buttonSize='btn--large'
                         >Join us
                         </Button>}
